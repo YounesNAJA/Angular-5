65704176318b4dba74c51c41273a7cbdf34a9139
Younes NAJA <naja.younes@gmail.com>
2018-02-03 22:34:09 +0000

    ngStyle and ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: 'server.component.html'
+    templateUrl: 'server.component.html',
+    styles: [`
+        .online {
+            color: white
+        }
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offline';
     serverName: string = '';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus() {
         return this.serverStatus;
@@ -15,5 +24,9 @@ export class ServerComponent {
 
     onUpdateServerName(event: Event) {
         this.serverName = event.target.value;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
